7037bbe56b57695f13e9d9d205019681643816cd
Shreyajaiswal11 <86391138+Shreyajaiswal11@users.noreply.github.com>
2022-10-05 19:13:23 +0530

    Create 6. Quick sort.cpp

---
+++ Sorting/6. Quick sort.cpp
@@ -0,0 +1,13 @@
+- its a divide and conquer algorithm.
+- it picks an element as pivot and partition the given array around the picked pivot.
+- different version of quick sort
+  - first element as pivot.
+  - last element as pivot.
+  - pick a random element as pivot.
+  
+- it is considered as in-place sorting algorithm. 
+  - In place sorting - a sort algorithm in which the sorted items occupy the same storage as the origional ones.
+  - this is usually achieved by swapping,shuffling,replacing and shifting of elements in the same array list.
+- it reduces the space complexity and removes the use of the auxilliary array that is used in merge sort.
+  T.C - o(nlogn)
+  S.P - o(log n)
